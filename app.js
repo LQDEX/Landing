cc2354734104eb5bf098b71843b4040f1122a4dd
cortesa <cortesa@gmail.com>
2018-03-18 23:34:59 +0100

    Connection error

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+var mysql = require("mysql");
+//Database connection
+app.use(function(req, res, next){
+	res.locals.connection = mysql.createConnection({
+		host     : '18.222.98.19',
+		user     : 'lqdex_user',
+		password : 'XTUdsQmYUD0lmiiA',
+		database : 'LQDEX'
+	});
+	res.locals.connection.connect();
+	next();
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  // res.locals.connection.query('SELECT * from users', function(error, results,fields){
+  //   if (error) throw error;
+  //   console.log(results);
+  //   res.send(JSON.stringify(results));
+  // });
   res.json([
     {id:1, username:'paco'},
     {id:2, username:'maria'}
