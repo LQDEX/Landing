40ee3e60fcbd8b15b4041443b642e35159526ffd
cortesa <cortesa@gmail.com>
2018-09-12 13:52:13 +0200

    Medium logo

--- client/src/constants/navBar.js
+++ client/src/constants/navBar.js
@@ -1,6 +1,14 @@
 export default {
   active: 'exchange',
-  options: [
+  menuDef: [
+    'press',
+    'aboutUs',
+    'whitepaper',
+    'contact',
+    'medium',
+    'telegram'
+  ],
+  items: [
     {
       name: 'press',
       type: 'btnMenu',
@@ -29,15 +37,6 @@ export default {
       pointTo: ''
     },
     {
-      name: 'blog',
-      type: 'link',
-      caption: 'BLOG',
-      class: 'btnMenu',
-      href: 'https://medium.com/lqdex',
-      pageTo: '',
-      pointTo: ''
-    },
-    {
       name: 'contact',
       caption: 'CONTACT',
       class: 'btnMenu',
@@ -48,10 +47,23 @@ export default {
     },
     {
       name: 'telegram',
-      type: 'linkIcon',
       caption: 'TELEGRAM ',
+      type: 'linkIcon',
+      icon: 'fa-telegram-plane',
+      title:'JOIN US ON TELEGRAM',
       class: 'btnMenu',
       href: 'https://t.me/LQDEX',
+      pageTo: '',
+      pointTo: ''
+    },
+    {
+      name: 'medium',
+      caption: '',
+      type: 'linkIcon',
+      icon: 'fa-medium-m',
+      title:'FOLLOW US ON MEDIUM',
+      class: 'btnMenu',
+      href: 'https://medium.com/lqdex',
       pageTo: '',
       pointTo: ''
     },
